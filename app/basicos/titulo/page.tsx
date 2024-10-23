cc090d7e6869102d5a1d614a4757d90c5a253846
Igor Bastos <114706936+IgorBastos19@users.noreply.github.com>
2024-10-23 09:08:36 -0300

    estudando

---
+++ app/basicos/titulo/page.tsx
@@ -0,0 +1,12 @@
+import Titulo from "../../../components/titulo";
+
+export default function usandoTitulo() {
+  return (
+    <div>
+      <Titulo
+        principal="Pagina de cadastro"
+        secundario="Incluir, alterar e excluir coisas"
+      />
+    </div>
+  );
+}
